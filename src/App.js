8c8411f5aba9faf8af5384596e1e911f090ea993
motokikasai <kmomoky@hotmail.com>
2020-03-24 00:01:30 +0100

    Organize structure of project via styles & assets folders, install node-sass, and start adding initial round of data from data.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./assets/logo.svg";
+import "./styles/App.scss";
+import data from "./data.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h2>People List</h2>
+      <ul>
+        {data.map(person => {
+          return (
+            <li key={person.id}>
+              <div className="profile">
+                <div className="full-name">
+                  <span>{person.firstName} </span>
+                  <span>{person.lastName} </span>
+                </div>
+                <p>{person.title}</p>
+              </div>
+              <div className="contact-info">
+                <span>{person.contact.phone}</span>
+                <span>{person.contact.email}</span>
+                <span>{person.contact.url}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      {/* <header className="App-header">
+          </ul>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +41,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
